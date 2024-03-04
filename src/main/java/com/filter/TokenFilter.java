a06e4503a2f0b2477c7fddc71f8123dba03de50a
VanshaSomani <vanshasomani0@gmail.com>
2024-03-04 10:08:30 +0530

    Private And Public URI in TOken Filter

--- src/main/java/com/filter/TokenFilter.java
+++ src/main/java/com/filter/TokenFilter.java
@@ -1,6 +1,8 @@
 package com.filter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Component;
 
@@ -14,6 +16,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class TokenFilter implements Filter {
+	private final List<String> publicEndpoints = Arrays.asList("/signup");
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
@@ -21,16 +24,24 @@ public class TokenFilter implements Filter {
 		System.out.println("Token filter called");
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
-		String token = req.getHeader("Token");
-		if(token == null) {
-			System.out.println("Token is not present");
-			res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-			res.getWriter().print("UNAUTHORIZED");
-			res.getWriter().flush();		
-		}
+		String requestURI = req.getRequestURI();
+		if (publicEndpoints.contains(requestURI)) {
+			System.out.println("Public URI");
+            chain.doFilter(request, response);
+        }
 		else {
-			System.out.println("TOken Present");
-			chain.doFilter(request, response);
+			String token = req.getHeader("Token");
+			System.out.println("Private URI");
+			if(token == null) {
+				System.out.println("Token is not present");
+				res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				res.getWriter().print("UNAUTHORIZED");
+				res.getWriter().flush();		
+			}
+			else {
+				System.out.println("TOken Present");
+				chain.doFilter(request, response);
+			}
 		}
 	}
 }
